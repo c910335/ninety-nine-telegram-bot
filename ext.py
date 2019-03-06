e6d172579e419e1fc8f4f4b0f04da769390ec865
Tatsiujin Chin <c910335@gmail.com>
2019-03-06 11:12:04 +0800

    better log

--- ext.py
+++ ext.py
@@ -21,14 +21,17 @@ def exec():
 
 def handle(handler_type, callback, name = None):
     def handler(bot, update):
+        message_or_callback_query = update.message or update.callback_query
+        message = update.message or update.callback_query.message
+        text_or_data = update.message and update.message.text or update.callback_query.data
+        user = message_or_callback_query.from_user
         try:
-            print(update.message or update.callback_query)
+            print('@{} ({} {}): {}'.format(user.username, message.chat.type.capitalize(), type(message_or_callback_query).__name__, text_or_data))
             callback(bot, update)
             exec()
         except Exception as e:
-            print(e)
-            message = update.message or update.callback_query.message
-            message.reply_text(Strings.FAIL)
+            print('({}) {}'.format(type(e).__name__, str(e)))
+            bot.send_message(Settings.ADMIN_ID, '@{} ({} {}): ({}) {}'.format(user.username, message.chat.type.capitalize(), type(message_or_callback_query).__name__, type(e).__name__ , str(e)))
     if handler_type == 'command':
         return CommandHandler(name or callback.__name__, handler)
     elif handler_type == 'callback_query':
